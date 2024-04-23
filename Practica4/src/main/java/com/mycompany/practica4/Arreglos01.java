8d64b0ee76f83a84ad80ce6ec9b15332c0218c93
unknown <apadilla2@utpl.edu.ec>
2024-04-23 09:30:20 -0500

    practica4

--- Practica4/src/main/java/com/mycompany/practica4/Arreglos01.java
+++ Practica4/src/main/java/com/mycompany/practica4/Arreglos01.java
@@ -11,20 +11,45 @@ package com.mycompany.practica4;
 public class Arreglos01 {
 
     public static void main(String[] args) {
-        Metodos obj = new Metodos(5);
-        Metodos obj1 = new Metodos(10);
-        int opc;
+        Metodos obj = new Metodos();
+        int opc, num,pos;
         do {
             opc = obj.menu();
             switch (opc) {
-                case 1 -> obj.leerLleno();
-                case 2 -> obj.leerSig();
-                case 3 -> obj.presentar();
-                case 4 -> obj1.presentar();
-                default -> {
-                }
+                case 1:
+                    obj.leerLleno();
+                    break;
+                case 2:
+                    obj.leerSig();
+                    break;
+                case 3:
+                    obj.presentar();
+                    break;
+                case 4:
+                    System.out.println("Numero a buscar?");
+                    num = obj.sc.nextInt();
+                    if (obj.secuencial(num) != -1) {
+                        System.out.println("Esta en posicion " + obj.secuencial(num));
+                    } else {
+                        System.out.println("No esta...");
+                    }
+                    break;
+                case 5:
+                    System.out.println("Numero a eliminar");
+                    num = obj.sc.nextInt();
+                    obj.eliminar(num);
+                    break;
+                case 6:
+                    System.out.println("Numero a insertar");
+                    num = obj.sc.nextInt();
+                    System.out.println("Posicion?");
+                    pos=obj.sc.nextInt();
+                    obj.insertar(num,pos);
+                    break;
+                default:
+                    break;
             }
-        } while (opc != 0);
-
+        } while (opc
+                != 0);
     }
 }
--- Practica4/src/main/java/com/mycompany/practica4/Metodos.java
+++ Practica4/src/main/java/com/mycompany/practica4/Metodos.java
@@ -3,44 +3,113 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.practica4;
+
 import java.util.Scanner;
+
 /**
  *
  * @author utpl
  */
 public class Metodos {
+
     Scanner sc;
     int[] myArray;
     int ev;
-    
-    public Metodos(){
-        this.sc=new Scanner(System.in);
-        this.myArray=new int[10];
-        this.ev=0;
+
+    public Metodos() {
+        this.sc = new Scanner(System.in);
+        this.myArray = new int[6];
+        this.ev = 0;
     }
-    public int menu(){
+
+    public int menu() {
         System.out.println("Insertar lleno            [1]");
         System.out.println("Insertar en espacio libre [2]");
         System.out.println("Presentar                 [3]");
+        System.out.println("Buscar sec...             [4]");
+        System.out.println("Eliminar                  [5]");
+        System.out.println("Insertar                  [6]");
         System.out.println("Salir                     [0]");
         return sc.nextInt();
     }
-    public void leerLleno(){
-        for(int i=0; i<myArray.length;i++){
-            System.out.println("Intertar elemento de posicion "+(i+1));
-            myArray[i]=sc.nextInt();
+
+    public void leerLleno() {
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println("Intertar elemento de posicion " + (i + 1));
+            myArray[i] = sc.nextInt();
+            if (i>0) {
+                if (myArray[i] == myArray[i - 1]) {
+                 System.out.println("Numero repetido");
+                 i--;
+                }
+            }
         }
-        ev=myArray.length;
+        ev = myArray.length;
     }
-    public void leerSig(){
-        if(ev<myArray.length){
+
+    public void leerSig() {
+        if (ev < myArray.length) {
             System.out.println("Inserte elemento: ");
-                    myArray[ev]=sc.nextInt();
-        }else
+            myArray[ev] = sc.nextInt();
+        } else {
             System.out.println("Arreglo lleno...");
+        }
     }
-    public void presentar(){
-        for(int i=0;i<ev;i++){
-        System.out.print(myArray[i]+", ");}
+
+    public int secuencial(int num) {
+        for (int i = 0; i < ev; i++) {
+            if (num == myArray[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void presentar() {
+        for(int i =0; i<ev; i++){
+            if(i>0){
+                if(myArray[i]<myArray[i-1]){
+                    myArray[i-1]=myArray[i];
+                    myArray[i]=myArray[i-1];
+                    i--;
+                }
+            }
+        }
+        for (int i = 0; i < ev; i++) {
+            System.out.print(myArray[i] + ", ");
+        }
+        System.out.println();
+    }
+
+    public void recorrerIzq(int pos) {
+        for (int i = pos; i < ev - 1; i++) {
+            myArray[i] = myArray[i + 1];
+        }
+    }
+
+    public void eliminar(int num) {
+        int pos = secuencial(num);
+        if (pos != -1) {
+            recorrerIzq(pos);
+            ev--;
+        } else {
+            System.out.println("No existe...");
+        }
+    }
+
+    public void recorrerDer(int pos) {
+        for (int i = ev; i > pos; i--) {
+            myArray[i] = myArray[i - 1];
+        }
+    }
+
+    public void insertar(int num, int pos) {
+        if (ev < myArray.length) {
+            recorrerDer(pos);
+            myArray[pos] = num;
+            ev++;
+        } else {
+            System.out.println("Arreglo lleno...");
+        }
     }
 }
